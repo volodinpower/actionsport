59c59b43f789f887909fb581409ca1a7492235a7
Кирилл Володин <kirillvolodin@Kirills-MacBook-Pro.local>
2025-06-12 20:42:00 +0400

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import ProductDetails from "./pages/ProductDetails";
+import AdminLogin from "./components/AdminLogin";
+import AdminPanel from "./components/AdminPanel";
 import "./index.css";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/panel" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
