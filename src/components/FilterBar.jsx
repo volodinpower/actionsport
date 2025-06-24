249898f2b28b356bc71669c2d18aeb57828d26ea
Кирилл Володин <kirillvolodin@Kirills-MacBook-Pro.local>
2025-06-24 18:48:14 +0400

    Your commit message

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from "react";
 import Select from "react-select";
 
 export default function FilterBar({
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Header.css";
 
 // --- Подменю для меню ---
