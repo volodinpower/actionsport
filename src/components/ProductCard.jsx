be60be4d5cafaed9b580ab1f2da2275f6a0b727d
Кирилл Володин <kirillvolodin@Kirills-MacBook-Pro.local>
2025-07-04 01:27:57 +0400

    Your commit message

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
+import { Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
